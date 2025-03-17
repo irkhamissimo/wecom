dfcea5ad98ccef811629ff89a6da95a0a9ed2813
IrkhamN <56974703+irkhamissimo@users.noreply.github.com>
2025-03-18 06:39:26 +0700

    add bootstrap
    add bootstrap and autoload url

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -3,9 +3,13 @@
 
 <head>
 	<title><?php echo $title; ?></title>
-</head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-4.6.2/css/bootstrap.min.css'); ?>"
+		</head>
 
 <body>
 
-	<hr />
-	Ini konten yang seharusnya dinamis
+	<nav class="navbar navbar-dark bg-dark">
+		<a class="<?php echo base_url(); ?>" class="navbar-brand">WEECOM</a>
+	</nav>
